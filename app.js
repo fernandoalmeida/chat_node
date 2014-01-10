bc62f822181a615d5e77ae4b4f6a4cdc97cf57c5
Fernando Almeida <fernando@fernandoalmeida.net>
2014-01-10 02:55:58 -0300

    Basic login and public messages

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-
+/* global console, require, process, __dirname, JSON, setTimeout */
 /**
  * Module dependencies.
  */
 
-var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
-var http = require('http');
-var path = require('path');
-
-var app = express();
+var express  = require('express');
+var routes   = require('./routes');
+var user     = require('./routes/user');
+var chat     = require('./routes/chat');
+var socketio = require('socket.io');
+var http     = require('http');
+var path     = require('path');
+var app      = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -29,8 +30,64 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
+app.get('/chat', chat.main);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = app.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+var io = socketio.listen(server);
+
+var clients = {};
+var sockets = {};
+
+io.sockets.on('connection', function(socket){
+
+  // Login
+  socket.on('set username', function(user){
+    if (clients[user] === undefined) {
+      clients[user] = socket.id;
+      sockets[socket.id] = user;
+      available(socket.id, user);
+      join(user);
+    } else if (clients[user] == socket.id) {
+      ignore(socket.id, user);
+    } else {
+      unavailable(socket.id, user);
+    }
+  });
+
+  // Message
+  socket.on('message', function(data){
+    io.sockets.emit('message', {
+      source:  data.source,
+      message: data.message,
+      target:  data.target
+    });
+  });
+
+});
+
+var available = function(sid, user){
+  console.log("available: " + user + " at " + sid);
+  io.sockets.sockets[sid].emit('welcome', {user: user, current_users: JSON.stringify(Object.keys(clients)), message: "Welcome " + user});
+};
+
+var unavailable = function(sid, user){
+  console.log("unavailable: " + user + " at " + sid);
+  if (io.sockets.sockets[sid] != undefined) {
+    io.sockets.sockets[sid].emit('error', {unavailable: true, message: "username unavailable"});
+  }
+};
+
+var join = function(user){
+  Object.keys(sockets).forEach(function(sid){
+    if (io.sockets.sockets[sid]) {
+      io.sockets.sockets[sid].emit('joined', {user: user, notify: true});
+    }
+  });
+};
+
+var ignore = function(sid, user) {
+  console.log('ignore');
+};
---
+++ public/javascripts/chat.js
@@ -0,0 +1,53 @@
+/* global io, $, console */
+var socket = io.connect("http://localhost:3000");
+var user;
+
+$(document).ready(function(){
+
+  $('#user').keypress(function(e){
+    if (e.keyCode == 13) {
+      user = this.value;
+      socket.emit('set username', user, function(data){
+	console.log('emit set username', data);
+      });
+      e.stopPropagation();
+      e.preventDefault();
+    }
+  });
+
+  $('#message').keypress(function(e){
+    if (e.keyCode == 13) {
+      var target = $('#users').val();
+      socket.emit('message', {
+	source: user,
+	target: target,
+	message: $(this).val()
+      });
+      $(this).val("");
+      e.stopPropagation();
+      e.preventDefault();
+    }
+  });
+
+  socket.on('welcome', function(data){
+    $("#feedback").html("<span style='color: green'>"+data.message+"</span>");
+    $('input#message').prop('disabled', false);
+    $('input#user').prop('disabled', true);
+  });
+
+  socket.on('joined', function(data){
+    $('#users').append($('<option value="'+data.user+'">'+data.user+'</option>'));
+    if (data.notify && data.user !== user) {
+      $('#chat').append('<p class="notify">--> ' + data.user + ' joined</p>');
+    }
+  });
+
+  socket.on('message', function(data){
+    $('#chat').append('<p>' + data.source + ': ' + data.message + '</p>');
+  });
+
+  socket.on('error', function(data) {
+    $("#feedback").html("<span style='color: red'>"+data.message+"</span>");
+  });
+
+});
